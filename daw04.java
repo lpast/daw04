5a0804caed6beeb20c7b333a15f63f6ad778ea93
lpast <77457594+lpast@users.noreply.github.com>
2021-01-31 22:47:53 +0100

    Add files via upload
    mi primer commit

---
+++ daw04.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+                         
+import java.util.Scanner;
+
+public class daw04 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Introduzca un numero por favor");
+        
+        Scanner entrada=new Scanner(System.in);
+        
+        int numero=entrada.nextInt();
+        
+        if(numero%2==0){
+            
+            System.out.println(" El numero  " + numero + " es divisible entre 2.");
+        }
+        else{
+             
+             System.out.println(" El numero " + numero + " no es divisible entre 2.");        
+        }   
+    }
+}
